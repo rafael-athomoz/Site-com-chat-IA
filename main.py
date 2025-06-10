a16c9c8bed93b2345a2c5d960254386f4d6ce65f
Caco0 <cacotpds@gmail.com>
2025-06-09 22:23:15 -0300

    `Added support for loading environment variables from .env file, using PyMuPDF to read PDFs, and analyzing PDF content using OpenAI API.`

--- main.py
+++ main.py
@@ -10,20 +10,29 @@
 
 import streamlit as st
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
+import fitz  # PyMuPDF para ler PDFs
+
+# Carrega variáveis do .env
+load_dotenv()
+
+# Lê a API KEY
+api_key = os.getenv("OPENAI_API_KEY")
 
 # Cria cliente OpenAI
-client = OpenAI(
-    api_key="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"
-)
+client = OpenAI(api_key=api_key)
+
 
 # Título da página
-st.write("# ChatBot com IA")
+st.write("# Análise de edital")
 
 # Inicializa memória
 if "lista_mensagens" not in st.session_state:
     st.session_state["lista_mensagens"] = []
 
 # Exibe histórico
+st.subheader("Chat bot da sua Empresa")
 for historico_mensagem in st.session_state["lista_mensagens"]:
     role = historico_mensagem["role"]
     content = historico_mensagem["content"]
@@ -46,6 +55,46 @@ if mensagem_usuario:
     resposta_ia = resposta_modelo.choices[0].message.content.strip()
 
     # Mostra resposta da IA
-    st.chat_message("assistant").write(resposta_ia)
+    with st.chat_message("assistant"):
+        with st.expander("Expandir resposta"):
+            st.write(resposta_ia)
     mensagem_ia = {"role": "assistant", "content": resposta_ia}
     st.session_state["lista_mensagens"].append(mensagem_ia)
+
+# __________________
+# Ler arquivo PDF
+# _________________
+
+st.subheader("Análise do arquivo PDF")
+arquivo_pdf = st.file_uploader("Selecione o arquivo PDF", type=["pdf"])
+
+if arquivo_pdf is not None:
+    # Ler arquivo PDF
+    arquivo_pdf = fitz.open(stream=arquivo_pdf.read(), filetype="pdf")
+    texto_pdf = ""
+    for pagina in arquivo_pdf:
+        texto_pdf += pagina.get_text()
+
+    st.write("## Texto do arquivo PDF")
+    st.markdown(
+        f"<div style='overflow-x:auto; overflow-y:auto; max-height:400px;'>{texto_pdf}</div>",
+        unsafe_allow_html=True,
+    )
+
+    pergunta_pdf = st.text_input("Digite sa pergunta em relação ao PDF: ")
+
+    if st.button("Enviar pergunta"):
+        if pergunta_pdf.strip() != "":
+            resposta_pdf = f"Com base no texto do PDF, a resposta é: \n\n {texto_pdf}"
+            resposta_pdf = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": "user", "content": resposta_pdf}],
+            )
+
+            resposta_pdf_ia = resposta_pdf.choices[0].message.content.strip()
+
+            with st.chat_message("assistant"):
+                with st.expander("Expandir resposta"):
+                    st.write(resposta_pdf_ia)
+        else:
+            st.warning("Digite uma pergunta!")
